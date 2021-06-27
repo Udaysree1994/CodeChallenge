837200023db017fb27b6457d8c775e151679aaa8
Udaysree Gopi <gopi.udaysree@gmail.com>
2021-06-27 19:45:26 +1000

    added xunit test project

---
+++ name.sorter/name.sorter.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using name.sorter;
+
+namespace name.sorter.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
