32097c944d17e023004ef599159f5a3e08fcc27f
Udaysree Gopi <gopi.udaysree@gmail.com>
2021-06-27 20:24:35 +1000

    added unit test

--- name.sorter/name.sorter.tests/UnitTest1.cs
+++ name.sorter/name.sorter.tests/UnitTest1.cs
@@ -1,14 +1,50 @@
 using System;
 using Xunit;
-using name.sorter;
+using namesorter;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace name.sorter.tests
 {
     public class UnitTest1
     {
+        NameSortingService nameSortingService = new NameSortingService();
+        public List<Name> sortedNames = new List<Name>();
+        List<Name> unsortedNames = new List<Name>();
+        string names = "";
+
+
+        public UnitTest1()
+        {
+            sortedNames.Add(new Name("Marin", "Alvarez"));
+            sortedNames.Add(new Name("Adonis Julius", "Archer"));
+            sortedNames.Add(new Name("Beau Tristan", "Bentley"));
+            sortedNames.Add(new Name("Hunter Uriah Mathew", "Clarke"));
+
+            unsortedNames.Add(new Name("Hunter Uriah Mathew", "Clarke"));
+            unsortedNames.Add(new Name("Beau Tristan", "Bentley"));
+            unsortedNames.Add(new Name("Adonis Julius", "Archer"));
+            unsortedNames.Add(new Name("Marin", "Alvarez"));
+
+            foreach (var name in sortedNames)
+            {
+                names += name.FirstName + name.LastName;
+            }
+
+        }
+
         [Fact]
         public void Test1()
         {
+            var sortedNamesFromService = nameSortingService.NameSorter(unsortedNames);
+            
+            string sortedNames = "";
+            foreach(var name in sortedNamesFromService)
+            {
+                sortedNames += name.FirstName + name.LastName;
+            }
+
+           Assert.Equal(names, sortedNames);
 
         }
     }
